d85bec53d6c98b869d31fed0837ee12bff515c35
chrnk-exe <ivan_kot2001@mail.ru>
2024-02-21 23:48:51 +0300

    [client] Dashboard menu + main page

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import AppRoutes from './components/AppRoutes';
-import { Provider } from "react-redux";
+import { Provider } from 'react-redux';
 import { store } from './store/store';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -10,14 +10,15 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+
 root.render(
-  <React.StrictMode>
-      <Provider store={store}>
-          <BrowserRouter>
-            <AppRoutes />
-          </BrowserRouter>
-      </Provider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<Provider store={store}>
+			<BrowserRouter>
+				<AppRoutes />
+			</BrowserRouter>
+		</Provider>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
