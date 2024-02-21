2f757ade985d8715432879c0cc0a004dcda8574c
chrnk-exe <ivan_kot2001@mail.ru>
2024-02-21 13:31:38 +0300

    client deps

---
+++ client/src/hooks/typedReduxHooks.ts
@@ -0,0 +1,5 @@
+import { useDispatch, TypedUseSelectorHook,useSelector } from 'react-redux';
+import type { AppDispatch, RootState } from '../store/store';
+
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const typedReduxHooks = () => useDispatch<AppDispatch>();
---
+++ server/src/app/app.js
@@ -0,0 +1,2 @@
+const frida = require('frida')
+
