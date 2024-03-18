903091666f9e69ef9033ed3065264c8e24c5ee2b
chrnk-exe <ivan_kot2001@mail.ru>
2024-03-19 01:21:51 +0300

    [client] client changes (minimal)

---
+++ client/src/components/pages/TestingPages/M1TestingPage.tsx
@@ -0,0 +1,13 @@
+import React, {FC} from 'react';
+import { Box, Button } from '@mui/material';
+
+const M1TestingPage: FC = () => {
+	return (
+		<Box display={'flex'} justifyContent={'center'} alignItems={'flex-start'}>
+			<Button>TEST УРААА</Button>
+			M1 Testing Page
+		</Box>
+	);
+};
+
+export default M1TestingPage;
