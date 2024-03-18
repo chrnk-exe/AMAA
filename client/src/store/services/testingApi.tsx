4e604261a44235e9342ea9bc307ef680584169d1
chrnk-exe <ivan_kot2001@mail.ru>
2024-03-19 01:21:51 +0300

    [client] client changes (minimal)

---
+++ client/src/store/services/testingApi.tsx
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const testingApi = createApi({
+	reducerPath: 'testingApi',
+	baseQuery: fetchBaseQuery({
+		baseUrl: 'http://localhost:31337/api/testing',
+		credentials: 'include'
+	}),
+	endpoints: (build) => ({
+		getAllTestingResults: build.query<any, void>({
+			query: () => ''
+		}),
+		getOneTestingResults: build.query<any, {testNumber: number}>({
+			query: (testNumber) => `/${testNumber}`
+		})
+	}),
+
+});
+
+export const {
+	useLazyGetAllTestingResultsQuery,
+	useLazyGetOneTestingResultsQuery
+} = testingApi;
