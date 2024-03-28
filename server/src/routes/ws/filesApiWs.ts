726aa8e598b95ddc07576202a48650b6c4485168
chrnk-exe <ivan_kot2001@mail.ru>
2024-03-28 18:31:13 +0300

    [server] fileBrowsing implementation

--- server/src/routes/ws/filesApiWs.ts
+++ server/src/routes/ws/filesApiWs.ts
@@ -1,9 +1,20 @@
 import { Server, Socket } from 'socket.io';
+import myCookieParse from '../../utils/cookieParse';
+import DeviceFileBrowser from './DeviceFileBrowser';
 
+// emit events:
+// fileContent, directoryContent
+// receive events:
+// listDirectories, fileContent
 export default function shellHandlers(io: Server, socket: Socket) {
+	if (socket.request.headers.cookie) {
+		const cookies = myCookieParse(socket.request.headers.cookie);
+		const deviceId = cookies.deviceId as string;
 
-	socket.on('listdirs', async () => {
-		console.log();
-	});
+		const FileBrowser = new DeviceFileBrowser(deviceId, socket);
+		socket.on('disconnect', () => {
+			FileBrowser.clear();
+		});
+	}
 
 }
