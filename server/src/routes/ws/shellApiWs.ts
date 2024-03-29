fde0f9653da34f76f27e682f8d950594bca3d562
chrnk-exe <ivan_kot2001@mail.ru>
2024-03-29 21:51:08 +0300

    [server] some changes

--- server/src/routes/ws/shellApiWs.ts
+++ server/src/routes/ws/shellApiWs.ts
@@ -1,12 +1,12 @@
-import { Server, Socket } from 'socket.io';
-import { ChildProcess, fork } from 'child_process';
+import {Server, Socket} from 'socket.io';
+import {ChildProcess, fork} from 'child_process';
 import myCookieParse from '../../utils/cookieParse';
 
 interface shell {
-	subprocess: ChildProcess
-	pid: number
-	output: string
-	deviceId: string
+	subprocess: ChildProcess;
+	pid: number;
+	output: string;
+	deviceId: string;
 }
 
 // Просто потому что могу
@@ -15,14 +15,14 @@ let count = 0;
 const shells: shell[] = [];
 
 export default function shellHandlers(io: Server, socket: Socket) {
-	if (socket.request.headers.cookie){
+	if (socket.request.headers.cookie) {
 		const cookies = myCookieParse(socket.request.headers.cookie);
 		const deviceId = cookies.deviceId as string;
 
 		socket.on('spawn', async () => {
 			count = count + 1;
 			const subprocess = fork(__dirname + '\\..\\..\\frida-services\\shellChild.js', [deviceId]);
-			const shellInstance: shell = { output: '', subprocess, pid: count, deviceId };
+			const shellInstance: shell = {output: '', subprocess, pid: count, deviceId};
 
 			shells.push(shellInstance);
 
@@ -30,10 +30,20 @@ export default function shellHandlers(io: Server, socket: Socket) {
 				// todo: Разобраться, что не так с data
 				// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 				// @ts-ignore
-				const commandOutput = Buffer.from(data).toString();
+				const commandOutput = Buffer.from(data as Buffer).toString();
+
 
 				shellInstance.output = commandOutput;
-				socket.emit('commandResult', { commandOutput, pid: shellInstance.pid });
+				socket.emit('commandResult', {commandOutput, pid: shellInstance.pid});
+
+			});
+
+			// todo: сделать хуйню чтоб отправлялся эмит с уведомлением проверь тельчик!
+			subprocess.on('error', (err) => {
+				console.log('ERROR', err);
+			});
+			subprocess.on('exit', (signal) => {
+				console.log('Signal', signal);
 			});
 
 			socket.emit('spawnedShell', JSON.stringify({pid: count, deviceId, output: ''}));
@@ -45,23 +55,22 @@ export default function shellHandlers(io: Server, socket: Socket) {
 		});
 
 
-
-		socket.on('command', async ({cmd, pid}: {cmd?: string, pid: string}) => {
+		socket.on('command', async ({cmd, pid}: { cmd?: string, pid: string }) => {
 			if (cmd && !Number.isNaN(Number(pid))) {
 				const shell = shells.find(shell => shell.pid === +pid && shell.deviceId === deviceId);
 				if (shell) {
-					const { subprocess } = shell;
+					const {subprocess} = shell;
 					subprocess.send(cmd);
 				}
 			}
 		});
 
 
-		socket.on('kill', async ({pid}: {pid: string}) => {
+		socket.on('kill', async ({pid}: { pid: string }) => {
 			if (!Number.isNaN(Number(pid))) {
 				const shell = shells.find(shell => shell.pid === +pid && shell.deviceId === deviceId);
 				if (shell) {
-					const { subprocess } = shell;
+					const {subprocess} = shell;
 					subprocess.send('exit');
 					subprocess.kill(9);
 					shells.filter(shell => shell.pid !== +pid);
@@ -75,12 +84,12 @@ export default function shellHandlers(io: Server, socket: Socket) {
 		socket.on('killAll', async () => {
 			shells.forEach((shell) => {
 				try {
-					const { subprocess } = shell;
+					const {subprocess} = shell;
 					subprocess.send('exit');
 					subprocess.kill(9);
 					socket.emit('killResult', {message: 'process killed successfully!', pid: shell.pid});
 				} catch (err) {
-					socket.emit('killResult', {message: `Error, or shell with pid ${shell.pid} not found` });
+					socket.emit('killResult', {message: `Error, or shell with pid ${shell.pid} not found`});
 				}
 
 			});
